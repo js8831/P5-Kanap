69245c149d4c4d63e617afc913630eb35047da26
Jean Soulié <kalashh7@gmail.com>
2022-07-24 23:43:59 +0200

    fix : change of comments and variables

--- P5-Dev-Web-Kanap-master/front/js/script.js
+++ P5-Dev-Web-Kanap-master/front/js/script.js
@@ -1,4 +1,4 @@
-// Requête GET à l'API
+// Requête GET à l'API via fetch
 fetch("http://localhost:3000/api/products")
   // Création de la promesse avec then et
   // typage des données en JSONs'il y a un resultat
@@ -13,7 +13,7 @@ fetch("http://localhost:3000/api/products")
   //et les affiche sur la console
   .then(function (values) {
     console.log(values);
-    //Création et insertion, en boucle, des div pour chaque valeur existante
+    //Création et insertion, en boucle, des div pour chaque valeur existante dans la boucle
     //en appelant la fonction "buildHtml"
     values.map(function (value) {
       buildHtml(value);
@@ -24,9 +24,13 @@ fetch("http://localhost:3000/api/products")
     console.log(error);
   });
 
-function buildHtml() {
-  const newCanape = document.createElement("div");
+//fct qui ajoute les div crée dans la balise section (dom ?)
+//ensuite avec inner on met les liens, images, descrtiption... grâce aux variables et propriétés ?
+function buildHtml(product) {
+  const newDiv = document.createElement("div");
   let section = document.getElementById("items");
-  section.appendChild(newCanape);
-  console.log(newCanape);
+  section.appendChild(newDiv);
+  newDiv.innerHTML = `<a href="./product.html?id=${product._id}"><article><img src="${product.imageUrl}" alt="${product.altTxt}"><h3 class="productName">${product.name}</h3><p class="productDescription">${product.description}</p></article></a>`;
+  console.log(newDiv);
+  console.log(product);
 }
