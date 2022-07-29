a48f0d0db8ef50cd52b816e0df780ef14e43568b
Jean Soulié <kalashh7@gmail.com>
2022-07-29 20:42:12 +0200

    fix : add comment, LS, function event

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -8,6 +8,7 @@ console.log(params2);
 let id = params2.get("id");
 console.log(id);
 
+// Récuperation des propriétés d'un canapé par ID
 fetch(`http://localhost:3000/api/products/${id}`)
   .then((res) => {
     if (res) {
@@ -17,16 +18,14 @@ fetch(`http://localhost:3000/api/products/${id}`)
   .then((values) => {
     console.log(values);
     buildHtml(values);
-    values.colors.map(function (value) {
-      addColors(value);
-      console.log(value);
-    });
+    // ajouter la fct pour l'event
+    addButtonEvent(values);
   })
   .catch((error) => {
     console.log(error);
   });
 
-//ajoute les propriétes des canapes récupérées avec fetch
+// Ajoute les propriétes (name, price, photo et description) d'un canape sur la page 2
 function buildHtml(product) {
   let name = document.getElementById("title");
   name.innerHTML = `${product.name}`;
@@ -37,10 +36,65 @@ function buildHtml(product) {
   let img = document.querySelector(".item__img");
   img.innerHTML = `<img src="${product.imageUrl}" alt="Photographie d'un canapé">`;
   console.log(img);
+  // Récuperation des couleurs d'un canapé
+  product.colors.map(function (color) {
+    addColors(color);
+    console.log(color);
+  });
 }
 
-//ajoute les couleurs une par une
+// Ajoute les couleurs une par une
 function addColors(choice) {
   let colors = document.getElementById("colors");
   colors.innerHTML += `<option value="${choice}">${choice}</option>`;
 }
+
+// Décrire la fct
+function addButtonEvent(pdts) {
+  let elt = document.getElementById("addToCart");
+  elt.addEventListener("click", function (event) {
+    // Ici ou plus bas comme dans le sandbox ?
+    event.preventDefault();
+    // checkInput () avec if else ?
+    addToLocalStorage(pdts);
+  });
+}
+
+// function checkInput (){} à faire
+
+// Décrire la fct
+function addToLocalStorage(productInCart) {
+  // Création de l'objet JS
+  let optionProduct = {
+    idProduct: productInCart._id,
+    quantityProduct: document.getElementById("quantity").value,
+    colorProduct: document.getElementById("colors").value,
+  };
+  console.log(optionProduct);
+  // A supprimer le dernier console log ?
+  // console.log(addCart);
+
+  // Création d'une variable interrogeant le LS de la présence d'une clé "product"
+  // En l'a convertissant du JSON à un objet JS pour l'afficher ???
+  let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
+  console.log(productInLocalStorage);
+  // Si oui :
+  if (productInLocalStorage) {
+    productInLocalStorage.push(optionProduct)
+    localStorage.setItem("product", JSON.stringify(productInLocalStorage));
+    console.log(productInLocalStorage);
+  }
+  // Si non :
+  else {
+    // Créer un tableau (array) dans le Dom
+    productInLocalStorage = [],
+    // Y mettre la variable "optionPoduct" qui contient un objet JS
+    productInLocalStorage.push(optionProduct),
+    // Convertir le contenu du array en JSON afin de pouvoir le mettre dans le LS 
+    localStorage.setItem("product", JSON.stringify(productInLocalStorage));
+    console.log(productInLocalStorage);
+  }
+}
+
+// Mettre une alerte quand un pdt est ajouté au panier
+// On peut supprimer le panier avec un setTimeOut ?
